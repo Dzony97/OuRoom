909a50cea3b77e4cd9a62e2d01a68152e572e79d
Patrk Palonka <patrykpalonka12@gmail.com>
2024-01-02 16:14:21 +0100

    Add function sign_up in views.py

--- OuRoom/accounds/views.py
+++ OuRoom/accounds/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . forms import CreateUserForm
 
-# Create your views here.
+def sign_up(request):
+
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST) # If yes, creating a CreateUserForm based on the data from the request.
+
+        if form.is_valid():
+            form.save() # If the form is valid, saving the user.
+            return redirect('log_in')
+
+    context = {'registerform': form} # Passing the form to the template for rendering.
+
+    return render(request, 'accounts/signup.html', context=context)
+
