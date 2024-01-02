559ecf6cd7347af361f0113c7a8d5e42c902da24
Patrk Palonka <patrykpalonka12@gmail.com>
2024-01-02 16:35:03 +0100

    Add templates in settings.py

--- OuRoom/OuRoom/settings.py
+++ OuRoom/OuRoom/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+import os
 
 # Application definition
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'OuRoom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
