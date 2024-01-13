402bfdaa28db80daf36ad4df5279350efec87199
Patrk Palonka <patrykpalonka12@gmail.com>
2024-01-13 16:58:56 +0100

    Modified data in test_account_register_form_succes

--- OuRoom/accounts/tests/test_account_register_form_success.py
+++ OuRoom/accounts/tests/test_account_register_form_success.py
@@ -4,19 +4,16 @@ import pytest
 @pytest.mark.django_db
 def test_account_register_form_success():
     # Arrange
-    form = CreateUserForm({"username": "TestUser",
-                           "email": "testuser@wp.pl",
-                           "first_name": "Test",
-                           "last_name": "User",
-                           "password1": "inferno123",
-                           "password2": "inferno123",
+    form = CreateUserForm({"username": "Patryk",
+                           "email": "patrykpalonka@wp.pl",
+                           "first_name": "Patryk",
+                           "last_name": "Palonka",
+                           "password1": "haslohaslo",
+                           "password2": "haslohaslo",
                            })
 
     # Action
     result = form.is_valid()
-
-    if not result:
-        print(form.errors)
 
     # Assert
     assert result is True
