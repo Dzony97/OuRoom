0ac5eec3942f39a6ff1c4472f62925963f762ebf
Patrk Palonka <patrykpalonka12@gmail.com>
2024-01-13 17:36:15 +0100

    Add test_account_register_form_invalid_duplicate_email and modified CustomUser model / email cant duplicate

--- OuRoom/accounts/tests/test_account_register_form_success.py
+++ OuRoom/accounts/tests/test_account_register_form_success.py
@@ -1,4 +1,5 @@
 from ..forms import CreateUserForm
+from ..models import CustomUser
 import pytest
 
 @pytest.mark.django_db
@@ -17,3 +18,23 @@ def test_account_register_form_success():
 
     # Assert
     assert result is True
+
+@pytest.mark.django_db
+def test_account_register_form_invalid_duplicate_email():
+    # Arrange
+    CustomUser.objects.create(email="patrykpalonka@wp.pl")
+
+    form = CreateUserForm({"username": "Patryk",
+                           "email": "patrykpalonka@wp.pl",
+                           "first_name": "Patryk",
+                           "last_name": "Palonka",
+                           "password1": "haslohaslo",
+                           "password2": "haslohaslo",
+                           })
+
+    # Action
+    result = form.is_valid()
+
+    # Assert
+    assert result is False
+    assert form.errors["email"][0] == "User with this Email already exists."
