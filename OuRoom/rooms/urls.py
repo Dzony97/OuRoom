6df4044f576a0db551d3ebc19e565d4e048a80cb
Patrk Palonka <patrykpalonka12@gmail.com>
2024-02-14 21:18:54 +0100

    Add ouroom.html linked this and mainroom

--- OuRoom/rooms/urls.py
+++ OuRoom/rooms/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('mainroom/', views.main_room, name='main_room'),
+    path('ouroom/', views.main_room, name='ouroom'),
 ]
--- OuRoom/rooms/views.py
+++ OuRoom/rooms/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 def main_room(request):
-
     return render(request, 'rooms/mainroom.html')
+
+def ouroom(request):
+    return render(request, 'rooms/ouroom.html')
