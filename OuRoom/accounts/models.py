500ad512be43476db45d052b4c7981b4a9557b21
Patrk Palonka <patrykpalonka12@gmail.com>
2024-02-17 21:35:15 +0100

    Add models Profil. Add MEDIA_ROOT/URL in settings. Register class Profil in admin site

--- OuRoom/accounts/models.py
+++ OuRoom/accounts/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     email = models.EmailField(blank=False, null=False,unique=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(default='userdef.jpg', upload_to='profile_pics')
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profil - Lokalizacja: {self.location} - Data urodzenia: {self.birth_date}'
+
+
