989fd0fcbef7d7f2d483f25d84754e836fcd2211
Patrk Palonka <patrykpalonka12@gmail.com>
2024-02-19 20:41:47 +0100

    Add test username isn't duplicate

--- OuRoom/accounts/tests/test_account_register_form_success.py
+++ OuRoom/accounts/tests/test_account_register_form_success.py
@@ -38,3 +38,24 @@ def test_account_register_form_invalid_duplicate_email():
     # Assert
     assert result is False
     assert form.errors["email"][0] == "User with this Email already exists."
+
+
+@pytest.mark.django_db
+def test_account_register_form_invalid_duplicate_username():
+    # Arrange
+    CustomUser.objects.create(username="TestUser")
+
+    form = CreateUserForm({"username": "TestUser",
+                           "email": "patrykpalonka@wp.pl",
+                           "first_name": "Patryk",
+                           "last_name": "Palonka",
+                           "password1": "haslohaslo",
+                           "password2": "haslohaslo",
+                           })
+
+    # Action
+    result = form.is_valid()
+
+    # Assert
+    assert result is False
+    assert form.errors["username"][0] == "A user with that username already exists."
