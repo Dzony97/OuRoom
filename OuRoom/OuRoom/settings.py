bc8cd5a482d229beda637f5cfb208866b407852a
Patrk Palonka <patrykpalonka12@gmail.com>
2024-02-26 22:25:20 +0100

    Add templates for all function password_reset. Password reset working

--- OuRoom/OuRoom/settings.py
+++ OuRoom/OuRoom/settings.py
@@ -136,4 +136,4 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development onlynfi
