8b98764086ca41a34ef7ea15832eb327b0224b32
Patrk Palonka <patrykpalonka12@gmail.com>
2024-03-02 18:32:33 +0100

    Add PostListView and PostDetailView

--- OuRoom/rooms/views.py
+++ OuRoom/rooms/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
+from .models import Post
+
+class PostListView(ListView):
+
+    model = Post
+    template_name = 'rooms/mainroom.html'
+    context_object_name = 'post_list'
+    ordering = ['-data_posts']
+
+class PostDetailView(DetailView):
+    model = Post
+
+
 
 def main_room(request):
     return render(request, 'rooms/mainroom.html')
