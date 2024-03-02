1c330652e044c4e4bb04bdbc9cc0d1af10a4059d
Patrk Palonka <patrykpalonka12@gmail.com>
2024-03-02 17:26:34 +0100

    Add model Post and model Comment

--- OuRoom/rooms/models.py
+++ OuRoom/rooms/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+class Comment(models.Model):
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=200)
+    time_release = models.DateTimeField(default=timezone.now)
+
+class Post(models.Model):
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    time_release = models.DateTimeField(default=timezone.now)
+    content = models.TextField()
+    image = models.ImageField()
+    comment = models.ManyToManyField(Comment, blank=True)
+
+
+
