b647fd569959f36630dc0f6c868803882310e441
Patrk Palonka <patrykpalonka12@gmail.com>
2024-04-04 20:44:09 +0200

    Add reply form i post_detail

--- OuRoom/rooms/views.py
+++ OuRoom/rooms/views.py
@@ -20,6 +20,7 @@ class PostDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['commentform'] = AddCommentForm()
+        context['commentreplyform'] = AddCommentReplyForm()
         return context
 
 class PostCreateView(LoginRequiredMixin, CreateView):
