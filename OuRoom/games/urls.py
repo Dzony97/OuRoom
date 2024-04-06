8262ae01dcaa700033965da9e54cd6efd0161e82
Patrk Palonka <patrykpalonka12@gmail.com>
2024-04-06 21:39:30 +0200

    Registration second game paper rock and scissors

--- OuRoom/games/urls.py
+++ OuRoom/games/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('games/snake', views.snake, name='snake'),
+    path('games/paper', views.paper_games, name='paper'),
 ]
 
 if settings.DEBUG:
--- OuRoom/games/views.py
+++ OuRoom/games/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def snake(request):
     return render(request, 'games/snake.html')
+
+def paper_games(request):
+    return render(request, 'games/paper.html')
