cfbf161528c0bddd3f29155f2c720e42461443e5
Patrk Palonka <patrykpalonka12@gmail.com>
2024-04-07 15:06:07 +0200

    Fixed problem with migrate ( no value on_delete ).Setting on_delete to SET_NULL.The username remains in the ranking even after the account is deleted.

--- OuRoom/games/models.py
+++ OuRoom/games/models.py
@@ -3,8 +3,9 @@ from django.conf import settings
 
 class Score(models.Model):
 
-    player = models.ForeignKey(settings.AUTH_USER_MODEL)
+    player = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
     point = models.IntegerField()
+    player_username = models.CharField(max_length=30) # Stores the username
 
     def __str__(self):
-        return f'{self.player.username} - {self.point}'
+        return f'{self.player_username} - {self.point}'
