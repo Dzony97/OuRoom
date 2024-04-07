34863cb611c69bb289a9bbac15fcc66e68daf8c1
Patrk Palonka <patrykpalonka12@gmail.com>
2024-04-07 14:56:50 +0200

    Created model Score too ranking in snake games

--- OuRoom/games/models.py
+++ OuRoom/games/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Score(models.Model):
+
+    player = models.ForeignKey(settings.AUTH_USER_MODEL)
+    point = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.player.username} - {self.point}'
